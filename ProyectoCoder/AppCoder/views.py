dab862d160b60c5ef8c5faf7bb323322b4212e78
Fabian Giaccossa <fgiaccossa@gmail.com>
2023-03-20 23:00:46 -0300

    Tercera entrega con errores

---
+++ ProyectoCoder/AppCoder/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from AppCoder.models import Curso
+
+
+def cursos(request):
+    all_cursos = Curso.objects.all()
+    context = {
+        "cursos": all_cursos
+    }
+    return render(request, "AppCoder/cursos.html", context=context)
+
+
+def crear_curso(request, nombre, camada):
+    save_curso = Curso(nombre=nombre, camada=int(camada))
+    save_curso.save()
+    context = {
+        "nombre": nombre
+    }
+    return render(request, "AppCoder/save_curso.html", context)
+
+
+def estudiantes(request):
+    return render(request, "base.html")
+
+
+def profesores(request):
+    return render(request, "base.html")
---
+++ ProyectoCoder/ProyectoCoder/urls.py
@@ -0,0 +1,38 @@
+"""ProyectoCoder URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+import AppCoder.models
+from AppCoder.models import Curso
+from AppCoder.models import Profesor
+from AppCoder.models import Estudiante
+from AppcCoder.models import Entregable
+
+
+
+urlpatterns = [
+from django.urls import path
+from AppCoder.views import *
+
+urlpatterns = [
+    path('cursos', cursos, name="AppCoderCursos"),
+    path('curso/<nombre>/<anio>', crear_curso, name="AppCoderCurso"),
+    path('estudiantes', estudiantes, name="AppCoderEstudiantes"),
+    path('profesores', profesores, name="AppCoderProfesores"),
+]
+
+]
